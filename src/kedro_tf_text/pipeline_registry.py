3588cb73a98f2ff7b8d2062d174e37a8f1364d53
Bell Eapen <github@gulfdoctor.net>
2022-05-29 17:31:07 -0500

    glove embedding pipeline

--- src/kedro_tf_text/pipeline_registry.py
+++ src/kedro_tf_text/pipeline_registry.py
@@ -3,6 +3,8 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline, pipeline
 
+from kedro_tf_text.pipelines.preprocess.pipeline import create_embedding_pipeline
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -10,4 +12,4 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": pipeline([])}
+    return {"__default__": create_embedding_pipeline()}
--- src/kedro_tf_text/pipelines/preprocess/pipeline.py
+++ src/kedro_tf_text/pipelines/preprocess/pipeline.py
@@ -5,7 +5,35 @@ generated using Kedro 0.18.1
 # No pipelines registered
 
 from kedro.pipeline import Pipeline, node, pipeline
-
+from kedro_tf_text.pipelines.preprocess.nodes import create_glove_embeddings, pickle_processed_text, json_processed_text
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([])
+
+
+def create_embedding_pipeline(datasets={}, **kwargs) -> Pipeline:
+    return pipeline([
+
+                    # node(
+                    #     pickle_processed_text,
+                    #     [datasets.get("text_data", "text_data"),
+                    #      datasets.get("parameters", "parameters")],
+                    #     datasets.get("processed_text", "processed_text"),
+                    #     name="pickle_processed_text"
+                    # ),
+                    node(
+                        json_processed_text,
+                        [datasets.get("text_data", "text_data"),
+                         datasets.get("parameters", "parameters")],
+                        datasets.get("vocab_json", "vocab_json"),
+                        name="create_vocab"
+                    ),
+                    node(
+                        create_glove_embeddings,
+                        [datasets.get("pretrained_embedding", "pretrained_embedding"),
+                         datasets.get("vocab_json", "vocab_json"), datasets.get("parameters", "parameters")],
+                        datasets.get("glove_embedding", "glove_embedding"),
+                        name="create_glove_embeddings"
+                    ),
+
+                    ])
