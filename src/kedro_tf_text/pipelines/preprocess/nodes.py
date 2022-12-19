22d88b852ca1a69825ac01288d7877eda0fcbd4f
Bell Eapen <github@gulfdoctor.net>
2022-12-19 16:18:50 -0600

    preprocess for BERT in progress

--- src/kedro_tf_text/pipelines/preprocess/nodes.py
+++ src/kedro_tf_text/pipelines/preprocess/nodes.py
@@ -19,6 +19,8 @@ import tensorflow as tf
 from tensorflow.keras import layers
 import string
 
+TAG_RE = re.compile(r'<[^>]+>')
+
 def clean_medical(text_list):
     text_list = [single_string.lower().strip() for single_string in text_list] # lower case & whitespace removal
     text_list = [re.sub(r'\d+', '', single_string) for single_string in text_list] # remove numerics
@@ -252,3 +254,97 @@ def csv_model(preprocessing_head, csv_data, inputs, parameters: Dict):
 
     model.fit(x=csv_features_dict, y=csv_labels, epochs=10)
     return model
+
+
+## Cleaning text functions for BERT ##
+# Ref: https://github.com/artelab/Image-and-Text-fusion-for-UPMC-Food-101-using-BERT-and-CNNs/blob/main/BERT_LSTM.ipynb
+
+def preprocess_text(sen: str) -> str:
+    # Removing html tags
+    sentence = remove_tags(sen)
+
+    # Remove punctuations and numbers
+    sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+
+    # Single character removal
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    # Removing multiple spaces
+    sentence = re.sub(r'\s+', ' ', sentence)
+
+    sentence = sentence.lower()
+
+    return sentence
+
+# https://numpy.org/doc/stable/reference/generated/numpy.vectorize.html
+vec_preprocess_text = np.vectorize(preprocess_text)
+
+def remove_tags(text: str) -> str:
+    return TAG_RE.sub('', text)
+
+# Preprocessing of texts according to BERT
+
+
+def _get_masks(text: str, max_length: int, tokenizer: Any):
+    """Mask for padding"""
+    tokens = tokenizer.tokenize(text)
+    tokens = ["[CLS]"] + tokens + ["[SEP]"]
+    length = len(tokens)
+    if length > max_length:
+        tokens = tokens[:max_length]
+    return np.asarray([1]*len(tokens) + [0] * (max_length - len(tokens)))
+
+
+vec_get_masks = np.vectorize(_get_masks, signature='(),(),()->(n)')
+
+
+def _get_segments(text: str, max_length: int, tokenizer: Any):
+    """Segments: 0 for the first sequence, 1 for the second"""
+    tokens = tokenizer.tokenize(text)
+    tokens = ["[CLS]"] + tokens + ["[SEP]"]
+    length = len(tokens)
+    if length > max_length:
+        tokens = tokens[:max_length]
+
+    segments = []
+    current_segment_id = 0
+    with_tags = ["[CLS]"] + tokens + ["[SEP]"]
+    token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    for token in tokens:
+        segments.append(current_segment_id)
+        if token == "[SEP]":
+            current_segment_id = 1
+    return np.asarray(segments + [0] * (max_length - len(tokens)))
+
+
+vec_get_segments = np.vectorize(_get_segments, signature='(),(),()->(n)')
+
+def _get_ids(text: str, max_length: int, tokenizer: Any):
+    """Token ids from Tokenizer vocab"""
+    tokens = tokenizer.tokenize(text)
+    tokens = ["[CLS]"] + tokens + ["[SEP]"]
+    length = len(tokens)
+    if length > max_length:
+        tokens = tokens[:max_length]
+
+    token_ids = tokenizer.convert_tokens_to_ids(tokens)
+    input_ids = np.asarray(token_ids + [0] * (max_length-length))
+    return input_ids
+
+
+vec_get_ids = np.vectorize(_get_ids, signature='(),(),()->(n)')
+
+def prepare_for_bert(text_array: List, max_length:int, tokenizer: Any):
+    """Prepares text for BERT"""
+    ids = vec_get_ids(text_array,
+                      max_length,
+                      tokenizer).squeeze()
+    masks = vec_get_masks(text_array,
+                          max_length,
+                          tokenizer).squeeze()
+    segments = vec_get_segments(text_array,
+                                max_length,
+                                tokenizer).squeeze()
+
+    return ids, segments, masks
