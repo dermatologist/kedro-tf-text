b9c92bf387aed090f0b695a797a16174ab427976
Bell Eapen <github@gulfdoctor.net>
2022-12-20 20:06:47 -0500

    fix: remove deeptable

--- src/kedro_tf_text/pipelines/preprocess/nodes.py
+++ src/kedro_tf_text/pipelines/preprocess/nodes.py
@@ -13,8 +13,8 @@ import pandas as pd
 import numpy as np
 from typing import Any, Callable, Dict, List, Tuple
 from keras.layers import Embedding
-from deeptables.models.deeptable import DeepTable, ModelConfig
-from deeptables.models.deepnets import DeepFM
+# from deeptables.models.deeptable import DeepTable, ModelConfig
+# from deeptables.models.deepnets import DeepFM
 import tensorflow as tf
 from tensorflow.keras import layers
 import string
@@ -123,24 +123,24 @@ def create_glove_embeddings(load_from_text_dataset: str, load_vocab_from_json: D
                      trainable=True
                      )
 
-def dt_tabular_model(csv_data:pd.DataFrame, parameters: Dict):
-    y = csv_data.pop(parameters['TARGET'])
-    csv_data.drop(parameters['DROP'], axis=1, inplace=True)
-    X = csv_data
-    conf = ModelConfig(
-        nets=DeepFM,  # same as `nets=['linear','dnn_nets','fm_nets']`
-        categorical_columns='auto',  # or categorical_columns=['x1', 'x2', 'x3', ...]
-        # can be `metrics=['RootMeanSquaredError']` for regression task
-        # metrics=['AUC', 'accuracy'],
-        auto_categorize=True,
-        auto_discrete=False,
-        embeddings_output_dim=20,
-        embedding_dropout=0,
-    )
-    dt = DeepTable(config=conf)
-    deepmodel, history = dt.fit(X, y)
-    # https://github.com/DataCanvasIO/DeepTables/blob/master/deeptables/models/deeptable.py
-    return deepmodel.model
+# def dt_tabular_model(csv_data:pd.DataFrame, parameters: Dict):
+#     y = csv_data.pop(parameters['TARGET'])
+#     csv_data.drop(parameters['DROP'], axis=1, inplace=True)
+#     X = csv_data
+#     conf = ModelConfig(
+#         nets=DeepFM,  # same as `nets=['linear','dnn_nets','fm_nets']`
+#         categorical_columns='auto',  # or categorical_columns=['x1', 'x2', 'x3', ...]
+#         # can be `metrics=['RootMeanSquaredError']` for regression task
+#         # metrics=['AUC', 'accuracy'],
+#         auto_categorize=True,
+#         auto_discrete=False,
+#         embeddings_output_dim=20,
+#         embedding_dropout=0,
+#     )
+#     dt = DeepTable(config=conf)
+#     deepmodel, history = dt.fit(X, y)
+#     # https://github.com/DataCanvasIO/DeepTables/blob/master/deeptables/models/deeptable.py
+#     return deepmodel.model
 
 ########## https://machinelearningmastery.com/develop-n-gram-multichannel-convolutional-neural-network-sentiment-analysis/
 # calculate the maximum document length
