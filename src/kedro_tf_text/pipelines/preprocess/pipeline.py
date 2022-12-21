c30dc073266d465965376818ed58118c0dd0547a
Bell Eapen <github_public@gulfdoctor.net>
2022-12-21 13:25:59 +0000

    pipelines refactored

--- src/kedro_tf_text/pipelines/preprocess/pipeline.py
+++ src/kedro_tf_text/pipelines/preprocess/pipeline.py
@@ -14,30 +14,31 @@ def create_pipeline(**kwargs) -> Pipeline:
     call this modular pipeline in your main pipeline with the following code:
     https://kedro.readthedocs.io/en/stable/nodes_and_pipelines/modular_pipelines.html
     """
-def create_embedding_pipeline(**kwargs) -> Pipeline:
+def create_glove_embedding_pipeline(**kwargs) -> Pipeline:
     return pipeline([
 
-                    # node(
-                    #     pickle_processed_text,
-                    #     [datasets.get("text_data", "text_data"),
-                    #      datasets.get("parameters", "parameters")],
-                    #     datasets.get("processed_text", "processed_text"),
-                    #     name="pickle_processed_text"
-                    # ),
                     node(
                         func=json_processed_text,
-                        inputs=["text_data"],
-                        parameters=["params:embeddings"],
+                        inputs=["text_data", "params:glove_embedding"],
                         outputs="vocab_json",
                         name="create_vocab"
                     ),
                     node(
                         func=create_glove_embeddings,
-                        inputs=["pretrained_embedding", "vocab_json"],
-                        parameters=["params:embeddings"],
+                        inputs=["pretrained_embedding", "vocab_json", "params:glove_embedding"],
                         outputs="glove_embedding",
                         name="create_glove_embeddings"
                     ),
 
-                    ])
+    ])
 
+def pickle_processed_text_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
+
+                    node(
+                        pickle_processed_text,
+                        inputs=["text_data", "params:pickle_processed_text"],
+                        outputs="processed_text",
+                        name="pickle_processed_text"
+                    ),
+    ])
