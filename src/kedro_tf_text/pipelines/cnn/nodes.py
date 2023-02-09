6a5f15ee5f3284524b70ffc03d98ab0ac5ab6873
Bell Eapen <github@gulfdoctor.net>
2023-02-09 16:06:24 -0600

    License headers

--- src/kedro_tf_text/pipelines/cnn/nodes.py
+++ src/kedro_tf_text/pipelines/cnn/nodes.py
@@ -1,12 +1,34 @@
 """
-This is a boilerplate pipeline 'model'
-generated using Kedro 0.18.1
+ Copyright 2023 Bell Eapen
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 """
+
+
+
 from typing import Dict
-from keras.layers import Activation, Input, Dense, Flatten, Dropout, Embedding
-from keras.layers.convolutional import Conv1D, MaxPooling1D
-from keras.layers import concatenate
+
 from keras import regularizers
+from keras.layers import (
+    Activation,
+    Dense,
+    Dropout,
+    Embedding,
+    Flatten,
+    Input,
+    concatenate,
+)
+from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.models import Model
 
 # https://github.com/faikaydin/medical-multimodal-with-transfer-learning/blob/master/cnn_model.py
